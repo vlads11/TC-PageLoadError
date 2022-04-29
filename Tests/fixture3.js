9f2cbe6f1ee6688996434c5287364a74d8ce7cb1
Vlad Shut <vlad.shut@zwift.com>
2022-04-28 21:56:53 -0700

    sample TC error with page loads

---
+++ Tests/fixture3.js
@@ -0,0 +1,19 @@
+import { Selector } from "testcafe";
+import { t } from "testcafe";
+import { test } from 'testcafe';
+
+
+let homeURL = 'https://uk.zwift.com';
+
+//slice0
+let slice0 = Selector('[class=\'image-with-text-overlay__banner columns one-whole image-crop-none\']').withText('WE BLEND FITNESS WITH ADVENTURE');
+
+fixture `Shopify HowZwiftWorkspage`
+    .page('about:blank')
+    .beforeEach(async t => {
+        await t.navigateTo(homeURL);
+    })
+
+test('Slice0 Get Started Button goes to Create Account page', async t => {
+    await t.click(slice0);
+})
